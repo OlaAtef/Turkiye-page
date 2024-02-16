8119692d95993b0e2d9c34af5bb9615f359b493f
Ola <ola736492@gmail.com>
2024-02-16 08:27:10 +0200

    ..

---
+++ Arabic-index/script.js
@@ -0,0 +1,29 @@
+let img = document.querySelector('.img');
+function phones(phone){
+    img.src = phone;
+}
+
+var imglinks = document.getElementsByClassName("img-links");
+var textcontents = document.getElementsByClassName("text-contents");
+
+function openimg( tabname ){
+
+for(imglink of imglinks){
+    imglink.classList.remove("active-img");
+}
+
+for(textcontent of textcontents){
+    textcontent.classList.remove("active-text");
+}  document.getElementById(tabname).classList.add("active-text");
+
+}
+
+var lang = document.querySelector(".lang .language");
+
+document.querySelector("#lang").onclick = () =>{
+    lang.classList.toggle("active");
+};
+
+
+
+
